5e64206bf4cd6f9e9fa7ab6bd600808c504eb7c8
Nash Bean <iBeanSoftware@gmail.com>
2018-05-21 13:55:49 -0400

    Start of FamilyWeb structure
    Added FamilyWeb header, where the structure that will be used to create a web page for family pages and all the family members.

---
+++ FamilyWeb.h
@@ -0,0 +1,23 @@
+#include "WebMake.h"
+
+strust SurnameDir
+{
+       u8str surname;
+       u8str dir;
+  //...     
+       
+};
+
+struct FamilyPage
+{
+       WebPage family_page;
+       
+       
+};
+
+struct MemberPage
+{
+       WebPage family_page;
+       
+       
+};
