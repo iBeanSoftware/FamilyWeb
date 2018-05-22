f6a544fbb92d480d24b97c3717774b206907e9a1
Nash Bean <iBeanSoftware@gmail.com>
2018-05-22 12:44:52 -0400

    added pic_link fields to structs

--- FamilyWeb.h
+++ FamilyWeb.h
@@ -28,6 +28,7 @@ struct EventPage
 {
        struct EventInfo
        {
+              u8str event_pic_link;
               u8str event_date;
               u8str event_type;
               u8str m_member_name;
@@ -41,6 +42,7 @@ struct FamilyPage
 {
   struct FamilyPageInfo
   {
+         u8str family_pic_link;
          u8str fathers_fathers_name, fathers_mothers_name;
          u8str mothers_fathers_name, mothers_mothers_name;
          u8str fathers_name, mothers_name;
@@ -48,6 +50,7 @@ struct FamilyPage
          
   };
        WebPage page;
+       
        
        
 };
