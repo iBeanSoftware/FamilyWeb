e1a165c65df69b80759ed0c6a479d428b782213f
Nash Bean <iBeanSoftware@gmail.com>
2018-05-22 15:50:24 -0400

    Update WebMake.h

--- WebMake.h
+++ WebMake.h
@@ -21,6 +21,7 @@ struct WebPage
        u8str page_title;
        u8text style_text;
        u8str stylesheet_filepath;
+       u8text scipt_text;
        u8str javascript_filepath;
         
        u8text head, body, footer;
