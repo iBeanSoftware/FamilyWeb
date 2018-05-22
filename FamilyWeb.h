734bea9a1d71f2d2ad92719e45ccae05f45aa718
Nash Bean <iBeanSoftware@gmail.com>
2018-05-22 13:35:55 -0400

    added name space to varible names

--- FamilyWeb.h
+++ FamilyWeb.h
@@ -1,9 +1,11 @@
+#include "../iBS_LIB/UTF-8_String.h"
 #include "WebMake.h"
+
 
 strust SurnameDir
 {
-       u8str surname;
-       u8str root_path;
+       iBS::u8str surname;
+       iBS::u8str root_path;
   //...     
        
 };
@@ -12,12 +14,12 @@ struct MemberPage
 {
   struct MemberPageInfo
   {
-         u8str profile_pic_link;
-         u8str prefix, first_name, middle_name, last_name, sufix;
-         u8str DoB, DoD;
-         u8str birth_place;
-         u8str fathers_name, mothers_name;
-         u8text other_info;
+         iBS::u8str profile_pic_link;
+         iBS::u8str prefix, first_name, middle_name, last_name, sufix;
+         iBS::u8str DoB, DoD;
+         iBS::u8str birth_place;
+         iBS::u8str fathers_name, mothers_name;
+         iBS::u8text other_info;
   };
        WebPage page;
        
@@ -28,12 +30,12 @@ struct EventPage
 {
        struct EventInfo
        {
-              u8str event_pic_link;
-              u8str event_date;
-              u8str event_type;
-              u8str m_member_name;
-              u8str f_member_name;
-              u8text other_info;
+              iBS::u8str event_pic_link;
+              iBS::u8str event_date;
+              iBS::u8str event_type;
+              iBS::u8str m_member_name;
+              iBS::u8str f_member_name;
+              iBS::u8text other_info;
        };
 };
 
@@ -42,11 +44,11 @@ struct FamilyPage
 {
   struct FamilyPageInfo
   {
-         u8str family_pic_link;
-         u8str fathers_fathers_name, fathers_mothers_name;
-         u8str mothers_fathers_name, mothers_mothers_name;
-         u8str fathers_name, mothers_name;
-         u8text children_names;
+         iBS::u8str family_pic_link;
+         iBS::u8str fathers_fathers_name, fathers_mothers_name;
+         iBS::u8str mothers_fathers_name, mothers_mothers_name;
+         iBS::u8str fathers_name, mothers_name;
+         iBS::u8text children_names;
          
   };
        WebPage page;
