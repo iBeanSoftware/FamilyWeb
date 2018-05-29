3340acda30b7f6c67e3bdfd733a6226d4a00660e
Nash Bean <iBeanSoftware@gmail.com>
2018-05-29 00:20:48 -0400

    adding populate_page()

--- FamilyWeb.h
+++ FamilyWeb.h
@@ -17,33 +17,38 @@ strust SurnameDir
 
 struct MemberPage
 {
-    struct MemberPageInfo
+    struct MemberInfo
     {
          iBS::u8str profile_pic_link;
          iBS::u8str prefix, first_name, middle_name, last_name, sufix;
          iBS::u8str DoB, DoD;
          iBS::u8str birth_place;
+         iBS::u8str death_place;
          iBS::u8str fathers_name, mothers_name;
          SurnameDir root_dir;
          iBS::u8text other_info;
     };
-       
+    MemberInfo info;   
     WebPage page;
        
     void populate_page()
     {
         page.head.append("<style> .image10p {width: 10%;}</style>");
            
-        page.body.append("<h1>" + last_name+" "+sufix+", "+prefix+" "+first_name+" "+middle_name + "</h1>");  
+        page.body.append("<h1>" + info.last_name+" "+info.sufix+", "+info.prefix+" "+info.first_name+" "+info.middle_name + "</h1>");  
         page.body.append("<h2> Parents </h2>");
-        page.body.append("<p> Father: "+fathers_name +"</p>");
-        page.body.append("<p> Mother: "+mothers_name +"</p>");
+        page.body.append("<p> Father: "+info.fathers_name +"</p>");
+        page.body.append("<p> Mother: "+info.mothers_name +"</p>");
+        page.body.append("<p> </p>");
         page.body.append("<img class=\"image10p\" src=/""+ profile_pic_link +"\" alt=\"Member Image\">");
-           page.body.append();
-           page.body.append();
-           page.body.append();
-           page.body.append();
+        page.body.append("<p>"+info.first_name+" "+info.middle_name+" "+info.last_name+" "+info.sufix+"</p>");  
+        page.body.append("<p> Date of Birth: "+info.DoB+" </p>");
+        page.body.append("<p> Place of Birth: "+info.birth_place+" </p>");
+        page.body.append("<p> Date of Death: "+info.DoD+" </p>");
+        page.body.append("<p> Place of Death: "+info.birth_place+" </p>");
+        page.body.append();
     };   
+ //void init(){};
  //bool add_member_page(){};
 };
 
@@ -51,15 +56,28 @@ struct EventPage
 {
        struct EventInfo
        {
+              SurnameDir root_dir;
               iBS::u8str event_pic_link;
               iBS::u8str event_date;
               iBS::u8str event_type;
-              iBS::u8str m_member_name;
-              iBS::u8str f_member_name;
-              SurnameDir root_dir;
+              iBS::u8str member_name;
               iBS::u8text other_info;
        };
+       
+       EventInfo info;
        WebPage page;
+       void populate_page()
+       {
+              page.head.append("<style> .image10p {width: 10%;}</style>");
+           
+              page.body.append("<h1>" + info.member_name+" "+info.event_type + "</h1>");  
+              page.body.append("<img class=\"image10p\" src=/""+ event_pic_link +"\" alt=\"Event Image\">");
+              page.body.append("<p> Event Date: "+info.event_date+" </p>");
+              for(int i=0; i<other_info.count; i++)
+              {
+              page.body.append("<p> "+info.other_info[i]+" </p>");
+              }
+    };        
  //bool add_event_page(){};
 };
 
